e72a23f74a713de06faa25fda1b04625225a618f
donsmith30 <donsmith30@hotmail.com>
2022-09-28 20:44:08 +0000

    mixed lists

---
+++ challenge1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - display data to std out"""
+
+def main():
+    name = input("Please enter name: ")
+    day  = input("Please enter day: ")
+    print("Hello, "+name+"! Happy "+day+"!")
+
+main()
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+print("The first item in the list (): "+ my_list[0])
+
+print("The seccond item in the list (pot): " +str(my_list[1]))
+
+print("The third item in the list (state): "+ my_list[2])
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print(iplist[3:5])
+
